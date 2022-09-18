42a63636ba32c1db769a611cf42e9e156c8f3b9b
iugstav <soaresgu2710@gmail.com>
2022-09-18 06:34:56 -0300

    documentation done

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ import (
 	"time"
 )
 
-func separateErrorMessage(err error) string {
+/*
+Separes the error message before and after the colon, then return the second part
+*/
+func SeparateErrorMessage(err error) string {
 	splittedError := strings.Split(err.Error(), "")
 	if pos := strings.Index(err.Error(), ":"); pos != -1 {
 		return strings.Join(splittedError[pos+2:], "")
@@ -19,7 +22,10 @@ func separateErrorMessage(err error) string {
 	return ""
 }
 
-func handleFileErrorMessage(errorMessage string) {
+/*
+A Switch case for possible file errors baseed on separateErrorMessage value
+*/
+func HandleFileErrorMessage(errorMessage string) {
 	switch errorMessage {
 	case "no such file or directory":
 		log.Fatal("This path does not reference nothing on the memory.")
@@ -28,9 +34,13 @@ func handleFileErrorMessage(errorMessage string) {
 	}
 }
 
+// limiter of directories per time
 var buffer = make(chan struct{}, 20)
 
-func handleDirOpening(dir string) []os.DirEntry {
+/*
+Handles the directory opening and return its contents
+*/
+func HandleDirOpening(dir string) []os.DirEntry {
 	buffer <- struct{}{}
 	defer func() {
 		<-buffer
@@ -44,11 +54,15 @@ func handleDirOpening(dir string) []os.DirEntry {
 	return items
 }
 
+/*
+LoopThroughDir function traverses all items inside the entered directory and
+if it has subdirectories the function will traverse them recursively
+*/
 func LoopThroughDir(dir string, wg *sync.WaitGroup, filesChannel chan int64, directoriesNumber *int64) {
 	time.Sleep(time.Millisecond * 100)
 
 	defer wg.Done()
-	for _, item := range handleDirOpening(dir) {
+	for _, item := range HandleDirOpening(dir) {
 		if item.IsDir() {
 			*directoriesNumber++
 
@@ -69,12 +83,12 @@ func LoopThroughDir(dir string, wg *sync.WaitGroup, filesChannel chan int64, dir
 func main() {
 	fileName := strings.Trim(os.Args[1], "")
 	if len(fileName) > 1 && strings.Split(fileName, "")[0] == "/" {
-		log.Fatalf("%s is an invalid name. Please insert another.", fileName)
+		log.Fatalf("%s is an invalid name. Please insert another.", fileName) // "/something" is invalid
 	}
 
 	info, err := os.Stat(fileName)
 	if err != nil {
-		handleFileErrorMessage(separateErrorMessage(err))
+		HandleFileErrorMessage(SeparateErrorMessage(err))
 	} else {
 		if info.IsDir() {
 			fileSizes := make(chan int64)
